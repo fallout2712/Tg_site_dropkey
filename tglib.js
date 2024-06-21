518a4e1f8c33b052e9ed7487428f52cc91ac89c4
Nikolay <fallout27121997@gmail.com>
2024-06-21 14:32:13 +0300

    fix
    fix

--- tglib.js
+++ tglib.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const tg = window.Telegram.WebApp;
 
     function SetUpTg() {
@@ -6,6 +6,22 @@ document.addEventListener("DOMContentLoaded", function() {
         tg.expand();
         tg.setHeaderColor("#000000");
         tg.setBackgroundColor("#000000");
+
+        const tgId = tg.initDataUnsafe?.user?.id;
+        const tgFn = tg.initDataUnsafe?.user?.first_name || "";
+        const tgLn = tg.initDataUnsafe?.user?.last_name || "";
+        const tgPhotoUrl = tg.initDataUnsafe?.user?.photo_url;
+        const tgLang = tg.initDataUnsafe?.user?.language_code;
+
+        const nickElement = document.querySelector(".Nick p");
+        if (nickElement) {
+            nickElement.textContent = `${tgFn} ${tgLn}`.trim();
+        }
+
+        const imgElement = document.querySelector(".Nick img");
+        if (imgElement && tgPhotoUrl) {
+            imgElement.src = tgPhotoUrl;
+        }
     }
 
     SetUpTg();
