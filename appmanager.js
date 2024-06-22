5ad843ff231e9d614d4b0e9126d0a3216f6f3100
Nikolay <fallout27121997@gmail.com>
2024-06-22 09:26:11 +0300

    fix
    fix

--- appmanager.js
+++ appmanager.js
@@ -4,7 +4,7 @@ let stoneForClick = 1;
 const stoneImg = document.getElementById('stone');
 const countStone = document.getElementById('countStone');
 
-stoneElement.addEventListener('click', function () {
+stoneImg.addEventListener('click', function () {
     stone += stoneForClick;
     countStone.textContent = stone;
     console.log("Текущее значение stone: " + stone);
